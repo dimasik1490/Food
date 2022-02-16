ea4c9000480041c4c8e06dc8a9a4ebf38d1b64cc
Dmytro <dimahaisiuk@gmail.com>
2022-02-16 18:32:14 +0200

    added loading spinner for modals

--- js/script.js
+++ js/script.js
@@ -213,7 +213,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const forms = document.querySelectorAll("form");
 
     const message ={
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо, скоро мы с Вами свяжемся!',
         failure: 'Что-то пошло не так...'
     };
@@ -226,11 +226,14 @@ window.addEventListener('DOMContentLoaded', function() {
         form.addEventListener("submit", (e)=>{
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-                  statusMessage.classList.add('status');
-                  statusMessage.textContent = message.loading;
+            const statusMessage = document.createElement('img');
+                  statusMessage.src = message.loading;
+                  statusMessage.style.cssText = `
+                    display: block;
+                    margin: 0 auto;
+                  `;
 
-            form.append(statusMessage);
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
