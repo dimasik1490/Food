6387bdcad260f5e021e8171aeb432451267ddc9d
Dmytro <dimahaisiuk@gmail.com>
2022-03-14 18:30:50 +0200

    adding fetch

--- js/script.js
+++ js/script.js
@@ -235,10 +235,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader("Content-type", "application/json");
-
             const formData = new FormData(form); 
 
             const object ={};
@@ -247,17 +243,22 @@ window.addEventListener('DOMContentLoaded', function() {
                 object[key] = value;
             });
 
-            request.send(JSON.stringify(object));
-            request.addEventListener("load", () => {
-                if(request.status === 200){
-                    console.log(request.response);
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+            .then(data=>{
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                }else{
-                    showThanksModal(message.failure);
-                }
-            });
+            }).catch(()=>{
+               showThanksModal(message.failure);
+            }).finally(()=>{
+                form.reset();
+            })
         })
     }
 
